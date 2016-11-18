a16e59de5d1eb3f4cde890038a88b4ef781d3c10
bruere_s <sebastien.bruere@epitech.eu>
2016-11-18 10:00:13 +0100

    OK

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,53 @@
 $(window).resize( function() {
 	console.log($(window).innerWidth());
 });
+
+$(document).ready( function() {
+	var slide = 0;
+
+	$('.btn-prev').click( function() {
+		slide = changeSlide(slide - 1);
+	});
+	$('.btn-suiv').click( function() {
+		slide = changeSlide(slide + 1);
+	});
+});
+
+var img_slide = ['https://s3.amazonaws.com/heroku-adfinitas-campaign/care-noel-2016/img-slide-1.jpg', 
+'https://s3.amazonaws.com/heroku-adfinitas-campaign/care-noel-2016/img-slide-2.jpg',
+'https://s3.amazonaws.com/heroku-adfinitas-campaign/care-noel-2016/img-slide-3.jpg'];
+var text_slide = [];
+var titre_slide = [];
+
+
+function changeSlide(slide) {
+	var i = 0;
+
+	if (slide == -1)
+		slide = 2;
+	else if (slide == 3)
+		slide = 0;
+
+	$('.slide-show').each( function() {
+		$(this).fadeOut(300);
+		if (i == slide) {
+			$(this).delay(300).fadeIn(300);
+		}
+		i++;
+	});
+	return slide;
+}
+
+function appearSlide(slide) {
+	$('.slide-show').each( function() {
+
+
+	});
+
+}
+
+
+
+
+
+
